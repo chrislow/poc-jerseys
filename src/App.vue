40720ebe1c82bad7d873fc43886d0c86ad7bfb2a
Christian Loew <cloew.at@gmail.com>
2024-02-29 15:14:39 +0100

    feat: inital commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import SingleJersey from './components/SingleJersey.vue'
+import PrimaryButton from './components/PrimaryButton.vue'
+import matches from './assets/json/matches.json'
+
+const match = ref(matches[0])
+
+const changeMatch = (index) => {
+  match.value = matches[index]
+}
+</script>
+
+<template>
+  <main class="w-[500px] mx-auto p-8">
+    <div class=" flex flex-col gap-4 mb-16">
+      <primary-button @click="changeMatch(0)">Match 1</primary-button>
+      <primary-button @click="changeMatch(1)">Match 2</primary-button>
+      <primary-button @click="changeMatch(2)">Match 3</primary-button>
+    </div>
+    <div class="flex flex-col gap-8">
+      <div class="flex gap-8">
+        <single-jersey :base="'#' + match.jerseys.home.player.base" :sleeve="'#' + match.jerseys.home.player.sleeve"
+                       :number="'#' + match.jerseys.home.player.number" :type="'#' + match.jerseys.home.player.type"
+                       :sleevelong="'#' + match.jerseys.home.player.sleevelong"
+                       :real="match.jerseys.home.player.real"></single-jersey>
+        <single-jersey :base="'#' + match.jerseys.away.player.base" :sleeve="'#' + match.jerseys.away.player.sleeve"
+                       :number="'#' + match.jerseys.away.player.number" :type="'#' + match.jerseys.away.player.type"
+                       :sleevelong="'#' + match.jerseys.away.player.sleevelong"
+                       :real="match.jerseys.away.player.real"></single-jersey>
+      </div>
+      <div class="w-full text-center font-bold">
+        <p>{{ match.title }}</p>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button type="button"
+            class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <slot></slot>
+    </button>
+</template>
